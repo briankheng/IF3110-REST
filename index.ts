6668493882b7555394796d3fe126e971a04656d0
Michael Leon Putra Widhi <leonmichael463@gmail.com>
2023-10-22 00:09:39 +0700

    init : rest-service init

---
+++ index.ts
@@ -0,0 +1,25 @@
+require('dotenv').config()
+import express from 'express'
+import cors from 'cors'
+import prisma from './src/prisma'  // use prisma as ORM
+
+// Import all handlers from src, including middlewares (to be made soon)
+// ...
+
+const app = express()
+const PORT = 8080
+const startDate = new Date()
+
+app.listen(PORT, () => {
+    console.log(`Server started at http://localhost:${PORT}`);
+    console.log(`using database url ${process.env.DATABASE_URL}`);
+})
+
+app.use(express.json())
+app.use(cors())
+
+app.get('/', (_, res) => {
+    res.send(`Server has started since ${startDate}`)
+})
+
+// API Handling, including methods and JWT using
