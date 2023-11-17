077fecdc5ed3b79931de2f57ed6b050604570afa
Brian Kheng <briankheng63@gmail.com>
2023-11-17 11:21:58 +0700

    refactor: docker soap communication

--- src/utils/SoapCaller.ts
+++ src/utils/SoapCaller.ts
@@ -18,8 +18,7 @@ class SoapCaller {
       "api-key": process.env.SOAP_API_KEY,
     };
 
-    const updatedUrl = this.url.replace("localhost", "127.0.0.1");
-    // const updatedUrl = this.url.replace("localhost", "0.0.0.0");
+    const updatedUrl = this.url.replace("localhost", "0.0.0.0");
 
     console.log("Request Headers:", headers);
 
